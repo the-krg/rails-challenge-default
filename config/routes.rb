55218937e95dba08b3f8a1c97f9b107a9c187fd3
Enzo Haguiwara <haguiwara68@gmail.com>
2024-03-29 03:15:19 -0300

    Add User API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
+
+  namespace :api, defaults: { format: :json } do
+    get '/users', to: 'users#index'
+    post '/users', to: 'users#create'
+  end
 end
