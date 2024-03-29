884a2888dffb3273fc8ef188ac95ee48c10d0ab7
Enzo Haguiwara <haguiwara68@gmail.com>
2024-03-29 15:42:04 -0300

    Add search method for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  ALLOWED_SEARCH_PARAMS = %w(email full_name metadata).freeze
+
   validates :email, :phone_number, :password, :key, presence: true
   validates :email, :phone_number, :key, :account_key, uniqueness: true
 
@@ -9,4 +11,10 @@ class User < ApplicationRecord
   validates :key, length: { maximum: 100 }
   validates :account_key, length: { maximum: 100 }
   validates :metadata, length: { maximum: 2000 }
+
+  def self.search(search_params)
+    allowed_params = search_params.select { |param| ALLOWED_SEARCH_PARAMS.include?(param) }
+
+    where(allowed_params)
+  end
 end
