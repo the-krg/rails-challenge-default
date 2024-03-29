707ea37d53f6853429ece525d174081306215f33
Enzo Haguiwara <haguiwara68@gmail.com>
2024-03-29 03:05:48 -0300

    Add User validations
    validations include: length, presence and uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  validates :email, :phone_number, :password, :key, presence: true
+  validates :email, :phone_number, :key, :account_key, uniqueness: true
+
+  validates :email, length: { maximum: 200 }
+  validates :phone_number, length: { maximum: 20 }
+  validates :full_name, length: { maximum: 200 }
+  validates :password, length: { maximum: 100 }
+  validates :key, length: { maximum: 100 }
+  validates :account_key, length: { maximum: 100 }
+  validates :metadata, length: { maximum: 2000 }
 end
